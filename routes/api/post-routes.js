787818390eef6554e8ab06934c7f39ddb439ac39
hitmaker <mwegter95@gmail.com>
2022-01-05 16:20:37 -0600

    can create and manipulate posts. Used Sequelize Models and model method upVote

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -6,7 +6,13 @@ const sequelize = require('../../config/connection');
 router.get('/', (req, res) => {
     console.log('===================');
     Post.findAll({
-        attributes: ['id', 'post_url', 'title', 'created_at'],
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT (*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
         order: [['created_at', 'DESC']],
         include: [
             {
@@ -28,8 +34,14 @@ router.get('/:id', (req, res) => {
       where: {
         id: req.params.id
       },
-      attributes: ['id', 'post_url', 'title', 'created_at'],
-      include: [
+      attributes: [
+        'id',
+        'post_url',
+        'title',
+        'created_at',
+        [sequelize.literal('(SELECT COUNT (*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
+    include: [
         {
           model: User,
           attributes: ['username']
@@ -65,32 +77,15 @@ router.get('/:id', (req, res) => {
 
   // PUT /api/posts/upvote
   router.put('/upvote', (req, res) => {
-      //create the vote
-    Vote.create({
-        user_id: req.body.user_id,
-        post_id: req.body.post_id
-    }).then(() => {
-        // then fing the post we just voted on
-        return Post.findOne({
-            where: {
-                id: req.body.post_id
-            },
-            attributes: [
-                'id',
-                'post_url',
-                'title',
-                'created_at',
-                // use raw mySQL aggregate function query to get a count of how many votes the post has and return it under the name `vote_count`
-                [
-                    sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
-                    'vote_count'
-                ]
-            ]
-        })
-        .then(dbPostData => res.json(dbPostData))
-        .catch(err => res.json(err));
-  });
+      // custom static method
+    Post.upvote(req.body, { Vote })
+    .then(updatedPostData => res.json(updatedPostData))
+    .catch(err => {
+      console.log(err);
+      res.status(400).json(err);
+    });
 });
+    
 
   router.put('/:id', (req, res) => {
       //expects id
