d9b2f9429c1108c3bf51b1c9b3232cc0ce7d1881
hitmaker <mwegter95@gmail.com>
2022-01-11 22:15:09 -0600

    added login and logout functionality with sessions

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
-const path = require('path')
+const path = require('path');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
 
 
 const app = express();
@@ -13,6 +27,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
