369716c32ac84734e97fefc5df997e3f45b7d5b3
hitmaker <mwegter95@gmail.com>
2022-01-12 00:05:52 -0600

    added in Comment functionality from 13.5, front end handlebars not finished

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Post, User, Vote } = require('../../models');
+const { Post, User, Vote, Comment } = require('../../models');
 const sequelize = require('../../config/connection');
 
 // get all posts
@@ -15,6 +15,14 @@ router.get('/', (req, res) => {
         ],
         order: [['created_at', 'DESC']],
         include: [
+            {
+              model: Comment,
+              attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+              include: {
+                model: User,
+                attributes: ['username']
+              }
+            },
             {
                 model: User,
                 attributes: ['username']
@@ -42,6 +50,14 @@ router.get('/:id', (req, res) => {
         [sequelize.literal('(SELECT COUNT (*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
     ],
     include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
         {
           model: User,
           attributes: ['username']
@@ -77,14 +93,17 @@ router.get('/:id', (req, res) => {
 
   // PUT /api/posts/upvote
   router.put('/upvote', (req, res) => {
-      // custom static method
-    Post.upvote(req.body, { Vote })
-    .then(updatedPostData => res.json(updatedPostData))
-    .catch(err => {
-      console.log(err);
-      res.status(400).json(err);
-    });
-});
+    // make sure the session exists first
+    if (req.session) {
+      // pass session id along with all destructured properties on req.body
+      Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
+        .then(updatedVoteData => res.json(updatedVoteData))
+        .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+        });
+    }
+  });
     
 
   router.put('/:id', (req, res) => {
